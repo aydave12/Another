80a1f922aa76a32b31790158b0534803e1003e0c
aydave12 <81455493+aydave12@users.noreply.github.com>
2021-07-20 20:56:13 -0400

    new commit

---
+++ secgrp.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.web.id
+    device_index         = 0
+  }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
